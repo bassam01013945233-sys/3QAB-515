9b508de7f454e468401cb02633fe5070e1a3761d
bassam01013945233-sys <bassam01013945233@gmail.com>
2026-06-08 03:09:18 +0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, bot }) => {
-  let watermark = 'VA';
+  let watermark = 'Aisa-v1;
   
   let quoted = {
     key: { fromMe: false, participant: '0@s.whatsapp.net', remoteJid: 'status@broadcast' },
@@ -12,16 +12,16 @@ FN:${watermark}
 TEL;type=CELL;waid=${num}:+${num}
 END:VCARD`;
 
-  let img = 'https://i.pinimg.com/originals/e2/21/20/e221203f319df949ee65585a657501a2.jpg';
+  let img = 'https://litter.catbox.moe/c6ikee.jpg';
   
   await conn.sendMessage(m.chat, {
     contacts: { displayName: watermark, contacts: [{ vcard }] },
     contextInfo: {
       forwardingScore: 2023,
       externalAdReply: {
-        title: '𝑇𝛨𝛯 𝛩𝑊𝛮𝛯𝑅',
+        title: 'Aisa-v1,
         body: watermark,
-        sourceUrl: 'https://whatsapp.com/channel/0029Vb3UUKz3QxS3bgWmTc3x',
+        sourceUrl: 'https://whatsapp.com/channel/0029VbD2pIvFXUuVFTTsek0J',
         thumbnailUrl: img,
         mediaType: 1,
         showAdAttribution: true,
